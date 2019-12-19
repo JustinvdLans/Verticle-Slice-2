aecb73bdad48391286389834c4a8da1708e1903e
JustinvdLans <42778559+JustinvdLans@users.noreply.github.com>
2019-12-19 11:32:50 +0100

    Add files via upload

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    float mousSensitivity;
+    
+    [SerializeField]
+    Transform player, playerGun;
+
+    void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        RotateCamera();
+    }
+
+    void RotateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        float rotAmountX = mouseX * mousSensitivity;
+        float rotAmountY = mouseY * mousSensitivity;
+
+        Vector3 rotPlayerGun = playerGun.transform.rotation.eulerAngles;
+        Vector3 rotPlayer = player.transform.rotation.eulerAngles;
+
+        rotPlayerGun.x -= rotAmountY;
+        rotPlayerGun.z = 0;
+        rotPlayer.y += rotAmountX;
+
+        playerGun.rotation = Quaternion.Euler(rotPlayerGun);
+        player.rotation = Quaternion.Euler(rotPlayer);
+
+    }
+}
